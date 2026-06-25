72653e3c1e761e5db7ad77b4be9e1687f277adde
Milena Kolmakova <milastark08@gmail.com>
2026-06-25 19:24:29 +0300

    update

--- bot.py
+++ bot.py
@@ -4,9 +4,7 @@ from handlers.profit import (
     router as profit_router
 )
 
-from handlers.edit_order import (
-    router as edit_order_router
-)
+
 
 from aiogram import Bot, Dispatcher
 
@@ -38,9 +36,6 @@ async def main():
     dp.include_router(free_dates_router)
     dp.include_router(
         profit_router
-    )
-    dp.include_router(
-        edit_order_router
     )
 
     await dp.start_polling(bot)
